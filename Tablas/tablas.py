a29ab2cff4a9e5f4de2363dfa77fdecbcce59157
luismoreno-io <luis@luismoreno.io>
2021-09-21 23:51:18 -0500

    Tablas practice

---
+++ Tablas/tablas.py
@@ -0,0 +1,13 @@
+
+tabla3list = []
+
+def tabla3():
+    for f in range(1,11):
+        for i in range(1, 11):
+            element = f, " x ", i, "= ", f * i
+            print (element)
+            
+        print("\n")
+
+tabla3()
+
